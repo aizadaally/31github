b48b023c1baf8779e4c25ce69eebef8a8b728a14
Aizada <91057556+aizadaally@users.noreply.github.com>
2023-12-11 21:32:15 +0600

    Add files via upload

--- Remove.cpp
+++ Remove.cpp
@@ -1,13 +1,29 @@
 class Solution {
 public:
-    int removeElement(vector<int>& nums, int val) {
-        int index = 0;
-        for(int i = 0; i< nums.size(); i++){
-            if(nums[i] != val){
-                nums[index] = nums[i];
-                index++;
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head==NULL  ||  head->next==NULL) return head;
+
+        ListNode*prev=NULL;
+        ListNode*temp=NULL;
+        ListNode*curr=head;
+
+        while(curr!=NULL){
+            if(prev==NULL){
+                prev=curr;
+                temp=prev;
+                curr=curr->next;
+                continue;
+            }
+            if(curr->val == prev->val){
+                curr=curr->next;
+                if(curr==NULL) prev->next=NULL;
+            }
+            else{
+                prev->next=curr;
+                prev=curr;
+                curr=curr->next;
             }
         }
-        return index;
+        return temp;
     }
 };
